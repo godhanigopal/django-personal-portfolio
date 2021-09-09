05bc83a39e1b25cf033a0244f510917b83a130bf
gopallgodhani <gopallgodhani@gmail.com>
2021-09-09 01:37:10 +0000

    removed old files

--- personalPortfolio/settings.py
+++ personalPortfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)g+_h$gqxerub3tmn)-lgnw3_*6w*g2#)4cg)^g=c7g#-_-w++'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gopal.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 #STATIC_FILES = []
 #STATICFILES_DIRS = ( "/portfolio/static", )
 
